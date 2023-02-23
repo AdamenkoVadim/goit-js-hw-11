714fc591c931ebb9779bd43e299535879bb22806
AdamenkoVadim <106696623+AdamenkoVadim@users.noreply.github.com>
2023-02-23 10:23:30 +0100

    Comm last changes

--- src/index.js
+++ src/index.js
@@ -4,18 +4,15 @@ import imageMarkup from './js/imageMarkup';
 import { Notify } from "notiflix";
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-console.log('33333');
 
 const searchForm = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
-//const loadMoreBtn = document.querySelector('.load-more');
 
 let page = 1;
 const per_page = 40;
 let searchQuery = null;
 
 searchForm.addEventListener('submit', onSearch);
-//loadMoreBtn.addEventListener('click', onLoadMore);
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
@@ -40,53 +37,7 @@ async function onSearch(e) {
     Notify.failure('Sorry, there are no images matching your search query. Please try again.');
   }
   appendImagesMarkup(images);
-  //loadMoreBtn.classList.remove('is-hidden');
 }
-
-
-
-/* function imageMarkup(images) {
-         return images.map(
-             ({
-             webformatURL,
-               largeImageURL,
-               likes,
-               views,
-               comments,
-               tags,
-               downloads,
-             }) => {
-               return `
-               <div class='photo-card'>
-           <a href='${largeImageURL}'><img
-               src='${webformatURL}'
-               alt='${tags}'
-               loading='lazy'
-             /></a>
-           <div class='info'>
-             <p class='info-item'>
-               <b>Likes</b>
-               ${likes}
-             </p>
-             <p class='info-item'>
-               <b>Views</b>
-               ${views}
-             </p>
-             <p class='info-item'>
-               <b>Comments</b>
-               ${comments}
-             </p>
-             <p class='info-item'>
-               <b>Downloads</b>
-               ${downloads}
-             </p>
-           </div>
-         </div>`
-             }
-           )
-           .join('');
-                               
-      } */
 
 function appendImagesMarkup(images) {
   gallery.insertAdjacentHTML('beforeend', imageMarkup(images));
@@ -105,11 +56,9 @@ window.addEventListener('scroll', () => {
       const totalPages = page * per_page;
       if (totalImages <= totalPages) {
         Notify.info("We're sorry, but you've reached the end of search results.");
-        //loadMoreBtn.classList.add('is-hidden');
       }
       appendImagesMarkup(images);
       page += 1;
-      console.log(page);
     }
   }
 }
